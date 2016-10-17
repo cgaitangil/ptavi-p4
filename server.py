9fd9c8111de0563cbe3669492a7948af090db686
cgaitangil <cgaitangil@gmail.com>
2016-10-17 21:04:12 +0200

    eco hecho

--- server.py
+++ server.py
@@ -8,23 +8,33 @@ import sys
 import socketserver
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+#    def __init__(self):
+ #       self.users = {}
+    
 
     def handle(self):
-        self.wfile.write(b"Hemos recibido tu peticion")
+        
+        print('')
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+            print("El cliente nos manda:", line.decode('utf-8'))
+            self.wfile.write(line)  # Escribimos en socket el mensaje recibido
+            print('Enviando eco:', line.decode('utf-8'))  # Server eco
+            print('')
             print('Dir_ip_cliente:', self.client_address[0])
-            print('Puerto_cliente:', self.client_address[1], '\n')            
-
+            print('Puerto_cliente:', self.client_address[1], '\n')
+            
+            
 if __name__ == "__main__":
     Puerto_serv = int(sys.argv[1])  # El pasado por terminal en cliente
-    serv = socketserver.UDPServer(('', Puerto_serv), EchoHandler)
+    serv = socketserver.UDPServer(('', Puerto_serv), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
         print("Finalizado servidor")
+        
+    Usuarios = {}

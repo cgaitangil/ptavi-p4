2dfbf3cfd57190d10a71a96ca2efa33a4184a730
Carlos Gaitan Gil <cgaitan@delta44.aulas.gsyc.urjc.es>
2016-10-21 12:34:11 +0200

    7. done

--- client.py
+++ client.py
@@ -8,19 +8,26 @@ import sys
 import socket
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-
-SERVER = str(sys.argv[1])
-PORT = int(sys.argv[2])
-LINE = sys.argv[3:]
-LINE = LINE[0].upper() + ' sip:' + LINE[1] + ' SIP/2.0' + '\r\n\r\n'
-
+try:
+    SERVER = str(sys.argv[1])
+    PORT = int(sys.argv[2])
+    LINE = sys.argv[3:]
+    Exp = 'Expires: ' + sys.argv[5] + '\r\n\r\n' # Cabecera Expires
+    LINE = LINE[0].upper() + ' sip:' + LINE[1] + ' SIP/2.0' + '\r\n' + Exp
+except:
+    Error = 'Usage: client.py ip puerto register sip_address expires_value'
+    sys.exit('\n' + Error + '\n')
+    
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
+    print('\r\n' + "Enviando:" + '\r\n' + LINE)
     my_socket.send(bytes(LINE, 'utf-8'))
     data = my_socket.recv(1024)
     print('Recibido:', data.decode('utf-8'))
+
+
+
    
 
 print("Socket terminado.")

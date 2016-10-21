70fa7794ffadf7b848c06203a9ed8cd23ed6bdcf
Carlos Gaitan Gil <cgaitan@delta44.aulas.gsyc.urjc.es>
2016-10-21 11:26:37 +0200

    6. done

--- client.py
+++ client.py
@@ -12,17 +12,15 @@ import socket
 SERVER = str(sys.argv[1])
 PORT = int(sys.argv[2])
 LINE = sys.argv[3:]
-LINE[0] = LINE[0].upper()   # Enviar REGISTER con upper
-LINE = str(' '.join(LINE))
-
+LINE = LINE[0].upper() + ' sip:' + LINE[1] + ' SIP/2.0' + '\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    my_socket.send(bytes(LINE, 'utf-8'))
     data = my_socket.recv(1024)
     print('Recibido:', data.decode('utf-8'))
-    
+   
 
 print("Socket terminado.")
